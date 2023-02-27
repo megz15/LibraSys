aa67da528bb1f0ee7bf49813bfc4a287e68f44b3
megz15 <meghraj.g16@gmail.com>
2023-02-27 15:32:09 +0530

    search books

--- server/server.ts
+++ server/server.ts
@@ -28,42 +28,10 @@ db.exec(`create table if not exists books (
     borrowCount integer default 0
 );`)
 
-function initBooks() {
-    const books = [
-        ['001-BLA.J', 'Lorem Ipsum', 'Math', 'H. Wells', 7, 7],
-        ['002-ZZZ.J', 'Dolor Sit Amet', 'History', 'J. Smith', 3, 1],
-        ['003-ABC.J', 'Consectetur Adipiscing Elit', 'Science', 'E. Brown', 5, 2],
-        ['004-DEF.J', 'Sed Do Eiusmod Tempor Incididunt', 'Fiction', 'S. Johnson', 10, 0],
-        ['005-GHI.J', 'Ut Labore Et Dolore Magna', 'Biography', 'M. Garcia', 2, 2],
-        ['006-JKL.J', 'Aliquam Consequat Ultricies Ligula', 'Philosophy', 'A. Lee', 8, 5],
-        ['007-MNO.J', 'Nulla Facilisi. Aenean Nec Eros', 'Politics', 'R. Clark', 6, 3],
-        ['008-PQR.J', 'Vestibulum Ante Ipsum Primis', 'Arts', 'G. Turner', 4, 2],
-        ['009-STU.J', 'In Faucibus Orci Luctus Et', 'Poetry', 'T. Nguyen', 1, 0],
-        ['010-VWX.J', 'Quisque Ut Ligula Velit', 'Education', 'K. Kim', 9, 8]
-    ]
-
-    const stmt = db.prepare(`insert into books values(?,?,?,?,?,?)`)
-    for (let i = 0; i < books.length; i++) {
-        stmt.run(books[i])
-    }
-}
-
 function createUser(email:string, fName:string, uName:string, pwd:string) {
     // Planning to use Mersenne Twister generator later for the UID instead of SQL autoincrement
     // to minimize the chances of malicious attacks based on sequential IDs
     db.exec(`insert into users values(${Date.now()}, '${email}', '${fName}', '${uName}', '${pwd}', 0, 0);`)
-}
-
-function showBooks():Book[] {
-    const stmt = db.prepare(`select * from books`)
-    const rows = stmt.all()
-    return rows as Book[]
-}
-
-function searchBooks(searchedBook:string, limit=30):Book[] {
-    const stmt = db.prepare(`select * from books where lower(books.bName) like lower('%'|| $searchedBook ||'%') limit $limit`)
-    const rows = stmt.all({searchedBook, limit})
-    return rows as Book[]
 }
 
 app.use(express.static(path.join(__dirname, '../svelte/public')));
@@ -75,13 +43,40 @@ app.get('/', (req, res) => {
 
 app.get('/api/initBooks', (req, res) => {
     var bookCount = db.prepare('select count(*) from books').get()['count(*)']
-    if (!bookCount) initBooks()
+    if (!bookCount) {
+        const books = [
+            ['001-BLA.J', 'Lorem Ipsum', 'Math', 'H. Wells', 7, 7],
+            ['002-ZZZ.J', 'Dolor Sit Amet', 'History', 'J. Smith', 3, 1],
+            ['003-ABC.J', 'Consectetur Adipiscing Elit', 'Science', 'E. Brown', 5, 2],
+            ['004-DEF.J', 'Sed Do Eiusmod Tempor Incididunt', 'Fiction', 'S. Johnson', 10, 0],
+            ['005-GHI.J', 'Ut Labore Et Dolore Magna', 'Biography', 'M. Garcia', 2, 2],
+            ['006-JKL.J', 'Aliquam Consequat Ultricies Ligula', 'Philosophy', 'A. Lee', 8, 5],
+            ['007-MNO.J', 'Nulla Facilisi. Aenean Nec Eros', 'Politics', 'R. Clark', 6, 3],
+            ['008-PQR.J', 'Vestibulum Ante Ipsum Primis', 'Arts', 'G. Turner', 4, 2],
+            ['009-STU.J', 'In Faucibus Orci Luctus Et', 'Poetry', 'T. Nguyen', 1, 0],
+            ['010-VWX.J', 'Quisque Ut Ligula Velit', 'Education', 'K. Kim', 9, 8]
+        ]
+    
+        const stmt = db.prepare(`insert into books values(?,?,?,?,?,?)`)
+        for (let i = 0; i < books.length; i++) {
+            stmt.run(books[i])
+        }
+    }
     res.send(`Initialized books: ${JSON.stringify(bookCount)}`);
 });
 
 app.get('/api/getBooks', (req, res) => {
-    const books = showBooks()
-    res.json(books)
+    const stmt = db.prepare(`select * from books`)
+    const rows = stmt.all()
+    res.json(rows)
+})
+
+app.get('/api/searchBooks', (req,res)=>{
+    let searchedBook = req.query.book
+    let limit = req.query.limit
+    const stmt = db.prepare(`select * from books where lower(books.bName) like lower('%'|| $searchedBook ||'%') limit $limit`)
+    const rows = stmt.all({searchedBook, limit})
+    res.send(rows)
 })
 
 app.listen(port, () => {
--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
   import type { Book } from '../../server/types'
   // import ShowBooks from './lib/ShowBooks.svelte'
   let books:Book[]
@@ -8,42 +7,38 @@
     .then(data => {
       books = data;
     });
+  
+  let searchTimer:NodeJS.Timeout
+  let searchedBook = '';
+
+  function fetchBooks() {
+      fetch(`/api/searchBooks?book=${searchedBook}&limit=50`)
+          .then((res) => res.json())
+          .then((data) => books = data)
+  }
+
+  function handleSearch(e:Event) {
+      clearTimeout(searchTimer)
+      searchTimer = setTimeout(() => {
+          const target = e.target as HTMLInputElement
+          searchedBook = target.value
+          fetchBooks()
+      }, 300);
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
   <h1>LibraSys</h1>
+
+  <input
+    type="search"
+    placeholder="Search..."
+    value={searchedBook}
+    on:keyup={handleSearch}
+  />
 
   <div class="card">
     <pre><code>{JSON.stringify(books, null, 2)}</code></pre>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
