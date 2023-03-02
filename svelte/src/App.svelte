a4d31a0fcb5686a7f4624876d3268b6d09218efd
megz15 <meghraj.g16@gmail.com>
2023-03-03 00:57:32 +0530

    Fixed routing

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,6 +1,12 @@
 <script>
     export let component
     export let data
+    
+    const routes = {
+        'Index': Index,
+        'About': About
+    }
+    
     import Index from "./pages/Index.svelte";
     import About from "./pages/About.svelte";
 </script>
@@ -8,9 +14,7 @@
 <!-- Uisng conditional rendering according to the route -->
 <!-- Edit: using svelte:component instead-->
 
-<svelte:component this={component} data={data}/>
-
-<!-- ^^^ causing error, fix tomorrow -->>
+<svelte:component this={routes[component]} data={data}/>
 
 <!-- <Index data = {data}/> -->
 <!-- {data} -->
